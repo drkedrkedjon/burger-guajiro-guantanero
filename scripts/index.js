a8cf081f16681f97b2500b86b45d255479eb763b
Sasa Savic Perusina <sasa@bubulazy.com>
2022-11-13 15:32:49 +0000

    Implementacion del ticket terminada

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,25 @@
 import {menuArray as dataArray} from './data.js'
+const ticketArray = []
+let descuento = false
 
+document.addEventListener('click', function(e) {
+  if(e.target.dataset.agregarticket) {
+    agregarProducto(e.target.dataset.agregarticket)
+  }
+  if(e.target.dataset.btnremover) {
+    ticketArray.length > 0 && removerProducto(e.target.dataset.btnremover)
+  }
 
+})
+function removerProducto(uuidProducto) {
+  const indice = ticketArray.findIndex( producto => producto.id === uuidProducto )
+  ticketArray.splice(indice, 1)
+  renderTicket()
+}
+function agregarProducto(uuidProducto) {
+  ticketArray.push(dataArray.find( producto => producto.id === uuidProducto ))
+  renderTicket()
+}
 function getProductosHTML(data) {
   let html = ''
   data.forEach(product => {
@@ -16,15 +35,79 @@ function getProductosHTML(data) {
           <p>${ingedientesHTML}</p>
           <p class="precio">${product.price}€</p>  
         </div>
-        <button class="add-btn">+</button>
+        <button data-agregarticket="${product.id}" class="add-btn">+</button>
       </div>
     `
   });
   return html
 }
+function getTicketHtml(data) {
+  const ticketActual = {
+    precio: 0,
+    tipoProducto: [],
+    cantidadDescuento: 0,
+    precioTotal: 0
+  }
+  let listadoHtml = ''
 
-function render() {
+  ticketArray.forEach( producto => {
+    ticketActual.precio += producto.price
+    ticketActual.tipoProducto.push(producto.type)
+    if( 
+      ticketActual.tipoProducto.some( tipo => tipo === 'food') &&
+      ticketActual.tipoProducto.some( tipo => tipo ===  'drink') 
+      ) {
+      descuento = true
+      ticketActual.cantidadDescuento = (ticketActual.precio / 100 * 10).toFixed(2)
+      ticketActual.precioTotal = ticketActual.precio - ticketActual.cantidadDescuento
+    } else {
+      descuento = false
+      ticketActual.precioTotal = ticketActual.precio
+    }
+
+    listadoHtml += `
+    <div class="producto-unico">
+      <h3>${producto.name}</h3>
+      <button data-btnremover="${producto.id}" class="btn-quitar">Remover</button>
+      <p>${producto.price}</p>
+    </div>
+    `
+  } )
+  const html = `
+  <h3>Su pedido</h3>
+  
+    <div id="producto-listado">
+    ${listadoHtml}
+    </div>  
+    <div class="totales">
+      <div class="totales-elemeto">
+        <h3>Precio:</h3>
+        <p>${ticketActual.precio}</p>
+      </div>
+      <div class="totales-elemeto">
+        <p id="des-disponibe" class="no-disponible">Descuento combo: <span>10%</span></p>
+        <p>${ticketActual.cantidadDescuento}</p>
+      </div>
+      <div class="totales-elemeto">
+        <h3>Precio total:</h3>
+        <p>${ticketActual.precioTotal}</p>
+      </div>
+      <button class="btn-pedido">Completar pedido</button>
+  </div>
+  `
+  return html
+}
+function renderProductos() {
   document.querySelector("#productos").innerHTML = getProductosHTML(dataArray)
 }
- 
-render()
+function renderTicket() {
+  
+  document.querySelector('#ticket').innerHTML = getTicketHtml(ticketArray)
+  const descuentoParafo = document.querySelector('#des-disponibe')
+  if (descuento) {
+    descuentoParafo.classList.remove('no-disponible')
+  } else if (!descuento) {
+    descuentoParafo.classList.add('no-disponible')
+  }
+}
+renderProductos()
