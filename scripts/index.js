1e481faf7e49b060ce3e2663c6e93829be26b8a2
Sasa Savic Perusina <sasa@bubulazy.com>
2022-11-15 13:17:09 +0000

    Proyecto terminado

--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,56 @@
-import {menuArray as dataArray} from './data.js'
+import { menuArray as dataArray } from './data.js'
+const dialog = document.querySelector('#dialog')
+
 const ticketArray = []
 let descuento = false
 
-document.addEventListener('click', function(e) {
-  if(e.target.dataset.agregarticket) {
+document.addEventListener('click', function (e) {
+  if (e.target.dataset.agregarticket) {
     agregarProducto(e.target.dataset.agregarticket)
   }
-  if(e.target.dataset.btnremover) {
+  if (e.target.dataset.btnremover) {
     ticketArray.length > 0 && removerProducto(e.target.dataset.btnremover)
   }
-
+  if (e.target.id === "btn-pedido") {
+    dialog.showModal()
+  }
+  if (e.target.id === 'btn-pagar') {
+    terminarPago()
+  }
 })
+function terminarPago() {
+  const nombre = document.querySelector("#nombre").value
+  ticketArray.length = 0
+  document.querySelector('#ticket').innerHTML = `
+    <div class="pago-exito-mensaje">
+      <h2>Gracias, ${nombre} tu pedido esta en camino...</h2>
+    </div>
+  `
+  dialog.close()
+
+  const inputs = document.querySelectorAll('input')
+  inputs.forEach(input => {
+    input.value = ''
+  })
+
+}
 function removerProducto(uuidProducto) {
-  const indice = ticketArray.findIndex( producto => producto.id === uuidProducto )
+  const indice = ticketArray.findIndex(producto => producto.id === uuidProducto)
   ticketArray.splice(indice, 1)
   renderTicket()
 }
 function agregarProducto(uuidProducto) {
-  ticketArray.push(dataArray.find( producto => producto.id === uuidProducto ))
+  ticketArray.push(dataArray.find(producto => producto.id === uuidProducto))
   renderTicket()
 }
 function getProductosHTML(data) {
   let html = ''
   data.forEach(product => {
     let ingedientesHTML = ''
-    product.ingredients.forEach( ingr => {
+    product.ingredients.forEach(ingr => {
       ingedientesHTML += `${ingr}, `
     })
+    ingedientesHTML = ingedientesHTML.slice(0, -2)
     html += `
       <div class="product">
         <p class="emoji">${product.emoji}</p>
@@ -50,13 +74,13 @@ function getTicketHtml(data) {
   }
   let listadoHtml = ''
 
-  ticketArray.forEach( producto => {
+  ticketArray.forEach(producto => {
     ticketActual.precio += producto.price
     ticketActual.tipoProducto.push(producto.type)
-    if( 
-      ticketActual.tipoProducto.some( tipo => tipo === 'food') &&
-      ticketActual.tipoProducto.some( tipo => tipo ===  'drink') 
-      ) {
+    if (
+      ticketActual.tipoProducto.some(tipo => tipo === 'food') &&
+      ticketActual.tipoProducto.some(tipo => tipo === 'drink')
+    ) {
       descuento = true
       ticketActual.cantidadDescuento = (ticketActual.precio / 100 * 10).toFixed(2)
       ticketActual.precioTotal = ticketActual.precio - ticketActual.cantidadDescuento
@@ -69,10 +93,10 @@ function getTicketHtml(data) {
     <div class="producto-unico">
       <h3>${producto.name}</h3>
       <button data-btnremover="${producto.id}" class="btn-quitar">Remover</button>
-      <p>${producto.price}</p>
+      <p>${producto.price}€</p>
     </div>
     `
-  } )
+  })
   const html = `
   <h3>Su pedido</h3>
   
@@ -82,17 +106,17 @@ function getTicketHtml(data) {
     <div class="totales">
       <div class="totales-elemeto">
         <h3>Precio:</h3>
-        <p>${ticketActual.precio}</p>
+        <p>${ticketActual.precio}€</p>
       </div>
       <div class="totales-elemeto">
         <p id="des-disponibe" class="no-disponible">Descuento combo: <span>10%</span></p>
-        <p>${ticketActual.cantidadDescuento}</p>
+        <p>${ticketActual.cantidadDescuento}€</p>
       </div>
       <div class="totales-elemeto">
         <h3>Precio total:</h3>
-        <p>${ticketActual.precioTotal}</p>
+        <p>${ticketActual.precioTotal}€</p>
       </div>
-      <button class="btn-pedido">Completar pedido</button>
+      <button id="btn-pedido" class="btn-pedido">Completar pedido</button>
   </div>
   `
   return html
@@ -101,7 +125,7 @@ function renderProductos() {
   document.querySelector("#productos").innerHTML = getProductosHTML(dataArray)
 }
 function renderTicket() {
-  
+
   document.querySelector('#ticket').innerHTML = getTicketHtml(ticketArray)
   const descuentoParafo = document.querySelector('#des-disponibe')
   if (descuento) {
